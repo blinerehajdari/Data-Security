c1be33d57e55af694844446ba33d54b9a86c2684
blinerehajdari <Blinera.hajdari@student.uni-pr.edu>
2022-05-30 23:44:02 +0200

    Base64

---
+++ Base64/Base64App/Base64App/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Encryption;
+using Decryption;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Base64App
+{
+    public partial class Form1 : Form
+    {
+        string Zgjedh = "Kodo";
+        Encryption.toBase64 Kodo = new Encryption.toBase64();
+        Decryption.fromBase64 Dekodo = new Decryption.fromBase64();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            groupBox1.ForeColor = Color.DarkOliveGreen;
+            groupBox2.ForeColor = Color.DarkOliveGreen;
+
+            rdbZgjedhKodo.Checked = true;
+            rdbImportTekst.Checked = true;
+        }
+        
+        private void rdbZgjedhDekodo_CheckedChanged(object sender, EventArgs e)
+        {
+            Zgjedh = "Dekodo";
+            btnDo.Text = Zgjedh;
+        }
+
+        private void rdbZgjedhKodo_CheckedChanged(object sender, EventArgs e)
+        {
+            Zgjedh = "Kodo";
+            btnDo.Text = Zgjedh;
+        }
+
+        private void rdbImportTekst_CheckedChanged(object sender, EventArgs e)
+        {
+            txtInputTekst.Enabled = false;
+            Browse.Enabled = true;
+
+            txtInputTekst.Text = "";
+            txtTekstiJuaj.Text = "";
+            txtOutputTekst.Text = "";
+        }
+
+        private void rdbShkruajTekst_CheckedChanged(object sender, EventArgs e)
+        {
+            Browse.Enabled = false;
+            txtInputTekst.Enabled = true;
+
+            txtInputTekst.Text = "";
+            txtTekstiJuaj.Text = "";
+            txtOutputTekst.Text = "";
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog browser = new OpenFileDialog();
+            browser.Title = "Direktiva për hapjen e fajllit!";
+
+            if (browser.ShowDialog() == DialogResult.OK)
+            {
+                txtInputTekst.Text = browser.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Zgjedh == "Kodo")
+            {
+                if (rdbImportTekst.Checked == true)
+                {
+                    StreamReader tekstiLexuar = new StreamReader(txtInputTekst.Text);
+
+                    txtTekstiJuaj.Text = tekstiLexuar.ReadToEnd();
+
+                    txtOutputTekst.Text = Kodo.Encode(txtTekstiJuaj.Text);
+
+                    tekstiLexuar.Close();
+                }
+                else
+                {
+                    txtTekstiJuaj.Text = txtInputTekst.Text;
+                    txtOutputTekst.Text = Kodo.Encode(txtInputTekst.Text);
+                }
+            }
+            else
+            {
+                if (rdbImportTekst.Checked == true)
+                {
+                    
+                    StreamReader tekstiLexuar = new StreamReader(txtInputTekst.Text);
+                    string teksti = tekstiLexuar.ReadToEnd();
+
+                    if (Dekodo.isStringValid(teksti))
+                    {
+
+                        txtTekstiJuaj.Text = teksti;
+
+                        txtOutputTekst.Text = Dekodo.Decode(teksti);
+                        tekstiLexuar.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Jo valid!");
+                        tekstiLexuar.Close();
+                        return;
+                    }
+                }
+                else
+                {
+                    txtTekstiJuaj.Text = txtInputTekst.Text;
+                    txtOutputTekst.Text = Dekodo.Decode(txtInputTekst.Text);
+                }
+            }
+        }
+
+        //nuk mundet me u fshi?!
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog browser = new SaveFileDialog();
+            browser.Filter = "Text Format|*.txt";
+            browser.Title = "Direktiva për ruajtjen e fajllit!";
+
+            if (browser.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter shkruajTekst = new StreamWriter(browser.FileName);
+
+                shkruajTekst.Write(txtOutputTekst.Text);
+                shkruajTekst.Close();
+
+
+                MessageBox.Show("Keni ruajtur me sukses tekstin!", "Informatë");
+            }
+        }
+    }
+}
---
+++ Base64/Decryption/Decryption/Class1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decryption
+{
+    public class fromBase64
+    {
+        //Konverton nje karakter me numer perkates me baze 64.
+        public int convertCharToInt(char character)
+        {
+            int found = 0;
+            string range = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+            for (int i = 0; i < range.Length; i++)
+            {
+                if (character == range[i])
+                {
+                    found = i;
+                    break;
+                }
+            }
+
+            return found;
+        }
+
+        StringBuilder first = new StringBuilder();
+        //Kthen nje numer te vetem ne binar.
+        public string ConvertIntToBinary(int numri)
+        {
+            if (numri == 0)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    first.Insert(0, 0);
+                }
+
+                return first.ToString(); //vec njo mjafton se tjerat i mush paddingu
+            }
+            if (numri == 1)
+            {
+                first.Insert(0, 1);
+
+                while (first.Length % 6 != 0)
+                {
+                    first.Insert(0, 0);
+                }
+
+                return first.ToString();
+            }
+            if (numri % 2 == 0)
+            {
+                first.Insert(0, 0);
+            }
+            if (numri % 2 == 1)
+            {
+                first.Insert(0, 1);
+            }
+
+            numri = numri / 2;
+            ConvertIntToBinary(numri);
+
+            return first.ToString();
+        }
+
+        StringBuilder second = new StringBuilder();
+        //Kthen nje tekst ne numra binar 6 bitesh.
+        public string convertTextToBinary(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                first.Clear();
+                int charPerText = convertCharToInt(text[i]);
+                second.Append(ConvertIntToBinary(charPerText));
+            }
+
+            return second.ToString();
+        }
+
+        StringBuilder third = new StringBuilder();
+        //Teksti i marrur nga funksioni convertTextToBinary
+        public string splitByEightBits(string text)
+        {
+            double sum = 0;
+            char pos = '1';
+
+            if (text.Length % 8 != 0)
+            {
+                text = text.PadRight(text.Length + (8 - text.Length % 8), '0');
+            }
+
+            for (int i = 0; i < text.Length; i = i + 8)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (text[i+j].Equals(pos))
+                    {
+                        sum = sum + Math.Pow(2, (7 - j));
+                    }
+                    else
+                    {
+                        sum = sum + 0;
+                    }
+                    
+                }
+
+                sum = Math.Floor(sum);
+                int fin = Convert.ToInt32(sum);
+                third.Append((char)fin);
+                sum = 0;
+            }
+
+            return third.ToString();
+        }
+        //Metoda finale qe kombinon metodat e mesiperme.
+        public string Decode (string text)
+        {
+            text = text.Replace("=", "");
+            string string1 = convertTextToBinary(text);
+            string string2 = splitByEightBits(string1);
+
+            first.Clear();
+            second.Clear();
+            third.Clear();
+
+            return string2;
+        }
+        //Kontrollon a eshte valid teksti per dekriptim.
+
+        public bool isStringValid (string text)
+        {
+            string range = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!range.Contains(text[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
